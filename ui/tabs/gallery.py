09ba2d145865159c0a55cafb9cee504ee254d527
Q657589025-ui <q657589025@gmail.com>
2026-08-04 22:39:03 +0800

    Update gallery.py

--- ui/tabs/gallery.py
+++ ui/tabs/gallery.py
@@ -1,1 +1,8 @@
+import gradio as gr
+from core.database import get_db_session, Work
 
+def create_gallery_tab(user_id_state):
+    with gr.Column():
+        gr.Markdown("## 📂 作品库")
+        gr.Markdown("加载作品...")
+    return gr.Column()
