b478b0a49455e3ea7dfb9232dffe79c301562581
Q657589025-ui <q657589025@gmail.com>
2026-08-04 22:38:27 +0800

    Update task_center.py

--- ui/tabs/task_center.py
+++ ui/tabs/task_center.py
@@ -1,1 +1,9 @@
+import gradio as gr
+from core.database import get_db_session, Task
 
+def create_task_center_tab(user_id_state):
+    with gr.Column():
+        gr.Markdown("## 📌 任务中心")
+        # 通过 user_id_state 变化刷新，此处占位
+        gr.Markdown("加载任务...")
+    return gr.Column()
